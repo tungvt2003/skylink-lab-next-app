eef18b3d0591fca51ea52ee704f0ae8b4371724b
TungVan57 <thientung57203@gmail.com>
2025-01-03 00:37:55 +0700

    fix: fix hover footer

--- src/lib/config/themes/skylink-lab/footer/RenderConfig.tsx
+++ src/lib/config/themes/skylink-lab/footer/RenderConfig.tsx
@@ -113,7 +113,7 @@ export const RenderConfig: ComponentConfig<FooterProps> = {
                       item.attribute.map((subItem, subIndex) => (
                         <a key={subIndex} href={subItem.url}>
                           <span
-                            className={`menu-item item-${index} flex items-center h-full text-[16px] font-semibold text-[#9E9E9E] transition-all duration-300`}
+                            className={`menu-item item-${index} flex items-center h-full text-[16px] font-semibold text-[#9E9E9E] hover:text-[#CD41FA] transition-all duration-300`}
                           >
                             {subItem?.title}
                           </span>
