863cea5647006779b5d643cec8c04831dd6ee49d
datvct <vancongthanhdata10@gmail.com>
2025-01-05 15:48:27 +0700

    fix: update metadata

--- src/app/[locale]/(root)/[...slugs]/page.tsx
+++ src/app/[locale]/(root)/[...slugs]/page.tsx
@@ -14,17 +14,20 @@ const Page = async (props: { params: Promise<{ slugs: string[]; locale: string }
   const params = await props.params
   const dict = await getDictionary(params.locale)
   const pageData = await fetchPageData(params.slugs, params.locale)
+
   return <PageRender pageData={pageData} hasBreadcrumb={true} dict={dict} />
 }
 
 export default Page
 
-export async function generateMetadata(context: { params: Promise<{ slugs?: string[] }> }): Promise<Metadata> {
+export async function generateMetadata(context: {
+  params: Promise<{ slugs?: string[]; locale: string }>
+}): Promise<Metadata> {
   const resolvedParams = await context.params
-  const { slugs = [] } = resolvedParams
+  const { slugs = [], locale } = resolvedParams
 
   try {
-    const pageData = await fetchPageData(slugs)
+    const pageData = await fetchPageData(slugs, locale)
     const metaData = pageData[0].attributes
 
     return generateMeta({ data: metaData })
